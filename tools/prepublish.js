d03139dbde2f181dcf01cab66af68d4bc67c8d7c
Max Nikitin <maxnik4u@gmail.com>
2017-11-26 14:08:48 +0300

    Add auto update minor version

--- tools/prepublish.js
+++ tools/prepublish.js
@@ -1,8 +1,13 @@
+const fs = require('fs')
+const path = require('path')
 const config = require('../package.json')
 let version = config.version.split('.')
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
   version[version.length - 1] = process.env.TRAVIS_BUILD_NUMBER
 }
+
+config.version = version.join('.')
+fs.writeFileSync(path.join(__dirname, '..', 'package.json'), JSON.stringify(config, false, '  '))
 
 process.stdout.write(`v${version.join('.')}`)
